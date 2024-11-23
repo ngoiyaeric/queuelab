9ecb2420e95e61cd09d634196cbb778f61551589
EreQ <info.ericmail@gmail.com>
2024-11-23 15:14:21 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,30 +6,24 @@ const nextConfig = {
             rule.test?.test?.(".svg")
         );
 
-        // Modify rule for PNG, JPEG, and SVG handling
         config.module.rules.push(
             // Reapply the existing rule, but only for svg imports ending in ?url
             {
                 ...fileLoaderRule,
-                test: /\.(svg|png|jpe?g)$/i, // Handle SVG, PNG, JPEG files
-                resourceQuery: /url/, // *.svg?url, *.png?url, *.jpg?url
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
             },
-            // Convert SVG files to React components (if not using the ?url query)
+            // Convert all other *.svg imports to React components
             {
                 test: /\.svg$/i,
                 issuer: fileLoaderRule.issuer,
                 resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
                 use: ["@svgr/webpack"],
-            },
-            // Ensure PNG and JPEG files are handled as URL imports (like the file loader does)
-            {
-                test: /\.(png|jpe?g)$/i,  // PNG and JPEG files
-                type: "asset/resource",  // This tells Webpack to treat them as file imports
             }
         );
 
-        // Modify the file loader rule to exclude *.svg, *.png, and *.jpeg files
-        fileLoaderRule.exclude = /\.(svg|png|jpe?g)$/i;
+        // Modify the file loader rule to ignore *.svg, since we have it handled now.
+        fileLoaderRule.exclude = /\.svg$/i;
 
         return config;
     },
