af2bc9bdc3510e97bb8d1fce0fb1c7b174d4b4ba
EreQ <info.ericmail@gmail.com>
2024-11-23 14:09:48 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,33 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack(config) {
-        // Grab the existing rule that handles SVG imports
+        // Add a rule for handling image files (png, jpg, etc.)
+        config.module.rules.push({
+            test: /\.(PNG|png|jpe?g|gif|webp|avif|ico)$/i,
+            type: "asset/resource",
+            generator: {
+                filename: "static/media/[name].[hash][ext]",
+            },
+        });
+
+        // Modify SVG handling as per your existing configuration
         const fileLoaderRule = config.module.rules.find((rule) =>
             rule.test?.test?.(".svg")
         );
 
         config.module.rules.push(
-            // Reapply the existing rule, but only for svg imports ending in ?url
             {
                 ...fileLoaderRule,
                 test: /\.svg$/i,
                 resourceQuery: /url/, // *.svg?url
             },
-            // Convert all other *.svg imports to React components
             {
                 test: /\.svg$/i,
                 issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
                 use: ["@svgr/webpack"],
             }
         );
 
-        // Modify the file loader rule to ignore *.svg, since we have it handled now.
         fileLoaderRule.exclude = /\.svg$/i;
 
         return config;
