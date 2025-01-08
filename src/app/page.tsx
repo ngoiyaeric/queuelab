4500b0f86456854e59302ee38b254422720c99b6
EreQ <info.ericmail@gmail.com>
2025-01-08 07:58:42 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <Features id="features" />
             <Testimonials />
             <PricingSection id="pricing" />
-            <CallToAction id="research" />
+            <CallToAction id="careers" />
             <GithubIndicator />
             <SiteFooter />
         </>
