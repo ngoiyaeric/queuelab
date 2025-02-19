af0cb71b710780cc47f8f7fb8994ef299011f17e
EreQ <info.ericmail@gmail.com>
2025-02-19 11:47:07 +0300

    Update page.tsx

--- src/app/reads/page.tsx
+++ src/app/reads/page.tsx
@@ -17,7 +17,8 @@ export default function ReadsPage() {
                         </p>
 
                         <p>
-                            This collective awareness of our environment ensures prompt error correction and predictions of potential threats through extensive inter-agent communication and context switching between sparse knowledge to identify root causes and plan for long-term mitigation for the context continuity and survival of our species. 43% of the people of Mongolia, for example, inhabit only 0.3% of the total environment to ensure efficient knowledge propagation within their society.
+                            This collective awareness of our environment ensures prompt error correction and predictions of potential threats through extensive inter-agent communication and context switching between sparse knowledge to identify root causes and plan for long-term mitigation for the context continuity and survival of our species. 
+                            
                         </p>
 
                         <p>
@@ -25,11 +26,11 @@ export default function ReadsPage() {
                         </p>
 
                         <p>
-                            Replenishing seems to be a central recurring indicator for flourishing societies, as the knowledge of depleting resources propagated with enough throughput onto the geographically distributed group of people as a result of being environmentally aware. Inhabitants from Easter Island who built everything with wood did so with little oversight to the point of absolute depletion with no replenishing and hence did not survive consecutive random floods and erosions that came with the environment.
+                            Replenishing seems to be a central recurring indicator for flourishing societies, as the knowledge of depleting resources propagated with enough throughput onto the geographically distributed group of people as a result of being environment aware. 
                         </p>
 
                         <p>
-                            As we continue to evolve into our digital environment, we diverge further from our physical one—spending increasing amounts of time indoors on our devices and having decreasing access and oversight on our physical environment. Democratizing access to environmental forecasts and digital replenishing is a great start to enhancing our experience as we become increasingly interconnected at the digital layer, as well as digitizing replenishing through physical-world interfaces.
+                            As we continue to evolve into our digital environment, we diverge further from our physical one—spending increasing amounts of time indoors on our devices and having decreasing access and oversight on our physical environment. Democratizing and intergrating access to environment forecasts and digital replenishing into our explorative systems is a great start to enhancing our experience as we become increasingly interconnected at the digital layer.
                         </p>
                     </div>
                 </div>  
